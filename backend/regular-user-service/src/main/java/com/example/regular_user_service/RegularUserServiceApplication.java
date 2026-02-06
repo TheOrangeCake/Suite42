1ef42666110f5f17d4d77f3756ff8990d63bc6d1
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-02-06 14:47:27 +0100

    chore: update api42 dependencies * Resolve CVE-2026-1225 * Resolve CVE-2026-24400

---
+++ backend/regular-user-service/src/main/java/com/example/regular_user_service/RegularUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.regular_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegularUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegularUserServiceApplication.class, args);
+	}
+
+}
---
+++ backend/regular-user-service/src/test/java/com/example/regular_user_service/RegularUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regular_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegularUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
