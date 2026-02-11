858184847cbb3bb76a1cb6f910f06a4135846010
Alex <alex.letondor@outlook.fr>
2026-02-11 20:49:31 +0100

    L component. 1st UI components with parameters: Horizontal size, vertical size, color and thickness. make it reutilisable everywhere.

---
+++ frontend/src/components/Corner.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  verticalLength: {
+    type: Number,
+    required: true
+  },
+  horizontalLength: {
+    type: Number,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  thickness: {
+    type: Number,
+    default: 3
+  }
+})
+
+const verticalStyle = computed(() => ({
+  height: props.verticalLength + 'px',
+  width: props.thickness + 'px',
+  backgroundColor: props.color
+}))
+
+const horizontalStyle = computed(() => ({
+  width: props.horizontalLength + 'px',
+  height: props.thickness + 'px',
+  backgroundColor: props.color,
+  top: props.verticalLength - props.thickness + 'px'
+}))
+</script>
+
+<template>
+  <div class="corner">
+    <div class="vertical" :style="verticalStyle"></div>
+    <div class="horizontal" :style="horizontalStyle"></div>
+  </div>
+</template>
+
+<style scoped>
+.corner {
+  position: relative;
+}
+
+.vertical {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+.horizontal {
+  position: absolute;
+  left: 0;
+}
+</style>
+
