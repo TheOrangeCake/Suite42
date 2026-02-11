d30e1ba07c686d60d8d04f236fabd21ebfd43d69
Alex <alex.letondor@outlook.fr>
2026-02-11 12:43:55 +0100

    simple navbar design without link to campuses page and signin page

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,21 +1,63 @@
+<script setup lang="ts">
+import logo from '/design/assets/logo/logo_icon.png'
+</script>
+
+
 <template>
   <v-app-bar
     app
     flat
-    color="white"
-    elevation="24"
-    density="comfortable"
+    color="#f3f3f3"
+    height="96"
+    class="px-10"
   >
-    <v-btn to="/">Home</v-btn>
-    <v-btn to="/about">About</v-btn>
-    <v-btn to="/signup">Guest Signup</v-btn>
+    <div class="brand">
+      <img  src="/design/assets/logo/logo_full.png"
+      class="logo"
+      />
+    </div>
+
+    <v-spacer />
+
+    <v-btn variant="text" class="nav-link">
+      Campuses
+    </v-btn>
+
+    <v-btn
+      color="#ff5a5a"
+      class="signin-btn"
+      rounded="lg"
+    >
+      Sign in
+    </v-btn>
+
   </v-app-bar>
 </template>
 
-<script setup lang= "ts">
+<style scoped>
+.brand {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
 
-</script>
+.logo {
+  margin-left: 40px;
+  opacity: 0.8;
+  height: 40px;
+}
 
-<style>
+.nav-link {
+  font-size: 18px;
+  margin-right: 24px;
+  text-transform: none;
+}
 
+.signin-btn {
+  font-size: 18px;
+  padding: 12px 22px;
+  text-transform: none;
+  color: white;
+}
 </style>
+
