cbbc57c3829beab419ed6028a6634b82c17310b7
Alex <alex.letondor@outlook.fr>
2026-02-27 20:01:56 +0100

    main page file and main login file

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,10 +1,33 @@
 <template>
+  <div class="page">
     <IndexSections/>
     <ToolkitSections/>
-    <AboutSections/>
-    <TechSections/>
+    <div class="dark-bg">
+      <AboutSections/>
+    </div>
+
+      <TechSections/>
+  </div>
 </template>
 
+<style scoped>
+.dark-bg {
+  background-color: #1a1a1a;
+  width: 100%;
+
+}
+.page {
+    padding: 30px 0px; /* ← garde le padding haut/bas, retire gauche/droite */
+  width: 100%;
+}
+
+
+@media (max-width: 868px ){
+  .page {
+      padding: 0px;
+  }
+}
+</style>
   <script>
     import TechSections from '../components/sections/IndexSections/TechSections.vue'
     import IndexSections from '../components/sections/IndexSections/IndexSections.vue'
